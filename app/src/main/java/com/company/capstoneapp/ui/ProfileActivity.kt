62239ec33d56500bef471b0475c9eae6795b36fe
Muhamad Fauzan <mfauzan120202@gmail.com>
2022-05-29 23:06:44 +0800

    fix: proses autentikasi regis user

--- app/src/main/java/com/company/capstoneapp/ui/ProfileActivity.kt
+++ app/src/main/java/com/company/capstoneapp/ui/ProfileActivity.kt
@@ -3,12 +3,19 @@ package com.company.capstoneapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.company.capstoneapp.R
+import com.company.capstoneapp.databinding.ActivityProfileBinding
 
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 
 }
--- app/src/main/java/com/company/capstoneapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/company/capstoneapp/ui/home/HomeActivity.kt
@@ -1,26 +1,17 @@
 package com.company.capstoneapp.ui.home
 
-import android.content.ContentValues.TAG
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
-import com.company.capstoneapp.ApiConfig
-import com.company.capstoneapp.DataFood
 import com.company.capstoneapp.R
 import com.company.capstoneapp.databinding.ActivityHomeBinding
 import com.company.capstoneapp.ui.adapter.ListCulinaryAroundAdapter
 import com.company.capstoneapp.ui.adapter.ListCulinaryRecommendationAdapter
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class HomeActivity : AppCompatActivity(){
+class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var userInfo: SharedPreferences
@@ -34,15 +25,23 @@ class HomeActivity : AppCompatActivity(){
         binding.apply {
             navbar.itemIconTintList = null
             tvUsername.text =
-                resources.getString(R.string.name_placeholder, userInfo.getString("name", "Unknown"))
+                resources.getString(
+                    R.string.name_placeholder,
+                    userInfo.getString("name", "Unknown")
+                )
             Glide.with(this@HomeActivity)
                 .load(userInfo.getString("urlPhoto", null))
                 .into(ivAvatar)
+
+            btnMaps.setOnClickListener {
+                startActivity(Intent(this@HomeActivity, HomeMapActivity::class.java))
+            }
         }
 
         binding.rvCulinaryAround.apply {
             setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(this@HomeActivity, LinearLayoutManager.HORIZONTAL, false)
+            layoutManager =
+                LinearLayoutManager(this@HomeActivity, LinearLayoutManager.HORIZONTAL, false)
             adapter = ListCulinaryAroundAdapter()
         }
 
@@ -51,35 +50,5 @@ class HomeActivity : AppCompatActivity(){
             layoutManager = LinearLayoutManager(this@HomeActivity)
             adapter = ListCulinaryRecommendationAdapter()
         }
-
-        val jsonObject = JsonObject()
-        jsonObject.addProperty("kind", "food")
-        jsonObject.addProperty("name", "bika_ambon")
-
-        val jsonPath = JsonObject()
-        val pathArray = JsonArray()
-        pathArray.add(jsonObject)
-        jsonPath.add("path", pathArray)
-
-        val jsonKey = JsonObject()
-        val keyArray = JsonArray()
-        keyArray.add(jsonPath)
-        jsonKey.add("key", keyArray)
-
-//        val dataFood = MutableLiveData<DataFood>()
-//        ApiConfig.getApiService("https://datastore.googleapis.com/v1/projects/")
-//            .getFood(jsonKey).enqueue(object : Callback<DataFood>{
-//                override fun onResponse(call: Call<DataFood>, response: Response<DataFood>) {
-//                    if (response.isSuccessful) {
-//                        Log.d( TAG, response.body().toString())
-//                        dataFood.postValue(response.body())
-//                    }
-//
-//                }
-//
-//                override fun onFailure(call: Call<DataFood>, t: Throwable) {
-//                }
-//
-//            })
     }
 }
